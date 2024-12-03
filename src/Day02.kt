83936b98b6baa806c958273d806ac61b81f82fff
Matthew Wilson <mfwilson1@gmail.com>
2024-12-02 16:45:51 -0800

    major improvements

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,18 +1,13 @@
 import java.io.File
-import kotlin.collections.sorted
-import kotlin.collections.sortedDescending
-import kotlin.math.abs
 
 fun main(args: Array<String>) {
     println("2024 Advent of Code day 2")
 
-    val reports = File("day2input.txt").readLines().map { line ->
-        line.split(" ").map { it.toInt() }
-    }
+    val reports = File("day2input.txt").readLines().map { line -> line.split(" ").map { it.toInt() } }
     println("There are ${reports.size} entries in the locations list")
 
-    // Part 1 - see if the lists are in ascending or descending order already
-    val part1 = reports.count { isSafe(it) }
+    // Part 1
+    val part1 = reports.count { isSafe(it).first }
     println("Part 1: The number of safe reports is $part1")
 
     // Part 2
@@ -20,55 +15,15 @@ fun main(args: Array<String>) {
     println("Part 2: The number of safe reports is $part2")
 }
 
-fun isSafe(report: List<Int>) : Boolean {
-    if (report.sorted() != report && report.sortedDescending() != report)
-        return false
-    if (report.windowed(2, 1).any { abs(it.first() - it.last()) > 3 || abs(it.first() - it.last()) < 1 })
-        return false
-    return true
+fun isSafe(report: List<Int>) : Triple<Boolean, Int, Int> {
+    val ascFailedIndex = report.windowed(2, 1).map { it[0] - it[1] >= -3 && it[0] - it[1] < 0 }.indexOfFirst { !it }
+    val descFailedIndex = report.windowed(2, 1).map { it[0] - it[1] <= 3 && it[0] - it[1] > 0 }.indexOfFirst { !it }
+    return Triple(ascFailedIndex == -1 || descFailedIndex == -1, ascFailedIndex, descFailedIndex)
 }
 
 fun isSafe2(report: List<Int>) : Boolean {
-    val ascending = isAscending(report)
-    var isSafe = false
-    var badIndex: Int = -1
-    report.windowed(2, 1).forEachIndexed { i, level ->
-        if (badIndex == -1) {
-            if (ascending) {
-                if (level.first() >= level.last()) {
-                    badIndex = i
-                } else if (level.last() - level.first() > 3) {
-                    badIndex = i
-                }
-            } else {
-                if (level.first() <= level.last()) {
-                    badIndex = i
-                } else if (level.first() - level.last() > 3) {
-                    badIndex = i
-                }
-            }
-        }
-    }
-
-    if (badIndex == -1) {
-        isSafe = true
-    } else {
-        // try removing first and second items from window and test
-        if (isSafe(report.filterIndexed { index, _ -> index != badIndex })) {
-            isSafe = true
-        } else if (isSafe(report.filterIndexed { index, _ -> index != badIndex + 1 })) {
-            isSafe = true
-        }
-    }
-
-    return isSafe
-}
-
-fun isAscending(report: List<Int>): Boolean {
-    var count = 0
-    if (report[0] < report[1]) count ++
-    if (report[1] < report[2]) count ++
-    if (report[2] < report[3]) count ++
-    if (report[3] < report[4]) count ++
-    if (count >= 2) return true else return false
+    val firstCheck = isSafe(report)
+    if (firstCheck.first) return true
+    val indicesToRemove = setOf(firstCheck.second, firstCheck.second + 1, firstCheck.third, firstCheck.third + 1)
+    return indicesToRemove.any { errorIndex -> isSafe(report.filterIndexed { i, _ -> i != errorIndex }).first }
 }
