545c39eeeac47cc9d2e7334343221679ce523382
Matthew Wilson <mfwilson1@gmail.com>
2024-12-04 22:31:08 -0800

    clean up day 5 part 1

--- src/Day05.kt
+++ src/Day05.kt
@@ -4,17 +4,13 @@ fun main(args: Array<String>) {
     println("2024 Advent of Code day 5")
 
     val input = File("day5input.txt").readText().split("\n\n").let { Pair(it[0], it[1]) }
-    val rules = input.first.lines().map { line -> line.split("|").map { it.toInt() } } //.let { Pair(it[0], it[1]) } }
+    val rules = input.first.lines().map { line -> line.split("|").map { it.toInt() } }
     val updates = input.second.lines().map { line -> line.split(",").map { it.toInt() } }
     println("There are ${rules.size} rules and ${updates.size} updates to process")
 
-    val part1 = updates.sumOf { update ->
-        if (rules.filter { update.containsAll(it) }.any { rule -> update.indexOf(rule[0]) > update.indexOf(rule[1]) })
-            0
-        else
-            update[update.size / 2]
-    }
-    println("$part1")
-
+    val part1 = updates.filterNot() { update ->
+        rules.filter { update.containsAll(it) }.any { rule -> update.indexOf(rule[0]) > update.indexOf(rule[1]) }
+    }.sumOf { it[it.size / 2] }
+    println("The sum of the middle page numbers of the correctly ordered updates is $part1")
 }
 
