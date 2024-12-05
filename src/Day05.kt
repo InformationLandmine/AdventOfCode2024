4e0b02b58cb2fb084a2c4f01408ded024077bf79
Matthew Wilson <mfwilson1@gmail.com>
2024-12-04 22:34:30 -0800

    formatting

--- src/Day05.kt
+++ src/Day05.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     val updates = input.second.lines().map { line -> line.split(",").map { it.toInt() } }
     println("There are ${rules.size} rules and ${updates.size} updates to process")
 
-    val part1 = updates.filterNot() { update ->
+    val part1 = updates.filterNot { update ->
         rules.filter { update.containsAll(it) }.any { rule -> update.indexOf(rule[0]) > update.indexOf(rule[1]) }
     }.sumOf { it[it.size / 2] }
     println("The sum of the middle page numbers of the correctly ordered updates is $part1")
